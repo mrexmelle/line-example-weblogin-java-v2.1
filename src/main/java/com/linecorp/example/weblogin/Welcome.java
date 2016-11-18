b631d2fb49148282d4d25b7efceb7880299f7f67
LINE <line@LINEs-MacBook-Pro.local>
2016-11-18 10:02:14 +0700

    Feature for Issue #1

---
+++ src/main/java/com/linecorp/example/weblogin/Welcome.java
@@ -0,0 +1,22 @@
+
+package com.linecorp.example.weblogin;
+
+import com.google.gson.Gson;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/line")
+public class Welcome
+{
+    @RequestMapping(value="/welcome", method=RequestMethod.GET)
+    public ResponseEntity<String> world()
+    {
+        final String html = "<html><body><form action=\"https://access.line.me/dialog/oauth/weblogin\" method=\"GET\"><input type=\"hidden\" name = \"response_type\" value=\"code\" /><input type=\"hidden\" name = \"client_id\" value=\"1479418979\" /><input type=\"hidden\" name = \"state\" value=\"x\" /><input type=\"hidden\" name = \"redirect_uri\" value=\"http://localhost:8080/line/auth\" /><input type=\"submit\" /></form></body></html>";
+        return new ResponseEntity<String>(html, HttpStatus.OK);
+    }
+};
